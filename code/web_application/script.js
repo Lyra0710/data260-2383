041e3334609a0b4d98a2e4613b060c9883cc837c
Lyra <ananyayallapragada@gmail.com>
2026-08-30 15:02:41 -0700

    js draft and html corrections

--- code/web_application/script.js
+++ code/web_application/script.js
@@ -3,15 +3,14 @@ const content = document.getElementById('description');
 const checkbox = document.getElementById('termsAccepted');
 
 const validateForm = (event) => {
+    event.preventDefault(); // to stop the default submission of the form
     if (content.value.trim().length <= 25) {
         alert("Description must be at least 26 characters long.");
-        return false;
     }
     if (!checkbox.checked) {
         alert("You must accept the terms and conditions.");
-        return false;
     }
-    return true;
+
 };
 
-form.addEventListener("submit", validateForm);
+form.addEventListener("submit", validateForm); // attaching the arrow function to the form's submit event
